e34c4cffaa3d68370022d95f86e1b4046438b88d
tangwind <windtcc@icloud.com>
2022-01-02 18:12:12 +0800

    类加载

--- src/jike/week1/ClassLoaderMain.java
+++ src/jike/week1/ClassLoaderMain.java
@@ -1,7 +1,17 @@
 package jike.week1;
 
+import java.lang.reflect.Method;
+
 public class ClassLoaderMain {
     public static void main(String[] args) {
-
+        MyClassLoader myClassLoader  = new MyClassLoader();
+        Class<?> hello = myClassLoader.findClass("Hello");
+        try {
+            Object obj = hello.newInstance();
+            Method method = hello.getMethod("hello");
+            method.invoke(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/jike/week1/MyClassLoader.java
+++ src/jike/week1/MyClassLoader.java
@@ -1,39 +1,42 @@
 package jike.week1;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
-public class MyClassLoader extends ClassLoader{
-    /**
-     * Finds the class with the specified <a href="#name">binary name</a>.
-     * This method should be overridden by class loader implementations that
-     * follow the delegation model for loading classes, and will be invoked by
-     * the {@link #loadClass <tt>loadClass</tt>} method after checking the
-     * parent class loader for the requested class.  The default implementation
-     * throws a <tt>ClassNotFoundException</tt>.
-     *
-     * @param name The <a href="#name">binary name</a> of the class
-     * @return The resulting <tt>Class</tt> object
-     * @throws ClassNotFoundException If the class could not be found
-     * @since 1.2
-     */
+public class MyClassLoader extends ClassLoader {
+
+
     @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
-        String myPath = "C:\\file\\Hello.xlass";
-        System.out.println(myPath);
-        byte[] cLassBytes = null;
-        Path path = null;
-        try {
-            path = Paths.get(new URI(myPath));
-            cLassBytes = Files.readAllBytes(path);
-        } catch (IOException | URISyntaxException e) {
+    public Class<?> findClass(String name) {
+        byte[] b = loadClassData(name);
+        assert b != null;
+        return defineClass(name, b, 0, b.length);
+    }
+
+    private byte[] loadClassData(String name) {
+        String path = "/Users/lulaaaa/IdeaProjects/JavaSource/src/jike/week1/";
+        name = path + name + ".xlass";
+        try (InputStream in = new FileInputStream(name);
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            int i = 0;
+            while ((i = in.read()) != -1) {
+                out.write(i);
+            }
+            byte[] bytes = out.toByteArray();
+            for (int j = 0; j < bytes.length; j++) {
+                bytes[j] = sub(bytes[j]);
+            }
+            return bytes;
+        } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
-        return defineClass(name, cLassBytes, 0, cLassBytes.length);
+    }
+
+    private byte sub(byte b) {
+        return (byte) (255 - b);
     }
 
 }
+
